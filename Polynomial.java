05770c4592b239b712ec96d9f92141de165a26b1
Saamur Ahmed <saamur19ua@gmail.com>
2025-10-01 21:03:51 -0400

    modified Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,36 +1,30 @@
-import java.io.*;
+//import java.io.*;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.io.BufferedReader;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.File;
-import java.util.Arrays;
-import java.io.BufferedReader;
-
-
-public class Polynomial{
-
-    double [] coeff;
+class Polynomial{
+//part a adding
+    double [] coefficient;
 	int [] expo;
     
     public Polynomial() {
 
-        coeff = new double[]{0.0};
+        coefficient = new double[]{0.0};
 		expo = new int[]{0};
     }
 
 
     public Polynomial(double [] arr_coeff, int [] arr_expo){
-        coeff = arr_coeff.clone();
+        coefficient = arr_coeff.clone();
 		expo = arr_expo.clone();
         
     }
+    
+    //constructor eith file
 
 	public Polynomial(File file) throws IOException{
 		BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -40,7 +34,7 @@ public class Polynomial{
 		String [] poly_arr = poly.split("(?=[+-])");
 		
 		int len = poly_arr.length;
-		coeff = new double[len];
+		coefficient = new double[len];
 		expo = new int[len];
 
 		for (int j = 0; j < poly_arr.length; j++) {
@@ -63,8 +57,7 @@ public class Polynomial{
 				} 
 				else if (coeffString.charAt(0) == '+')
 					coefficent = Double.parseDouble(coeffString.substring(1));
-				else
-					coefficent = Double.parseDouble(coeffString);
+				else coefficent = Double.parseDouble(coeffString);
 				
 
 				if (index_of_x == substrings.length() - 1) {
@@ -78,29 +71,28 @@ public class Polynomial{
                 	coefficent = Double.parseDouble(substrings.substring(1));
 				else
 					coefficent = Double.parseDouble(substrings);
-
                 exponent = 0;
             }
-
-			
-            coeff[j] = coefficent;
+            coefficient[j] = coefficent;
             expo[j] = exponent;
 
 		}
 	}
+	
+	
 
     public Polynomial add(Polynomial poly){
         int len = 0;
 		
-		for (int i = 0; i < coeff.length; i++){
-			for (int j = 0; j < poly.coeff.length; j++){
+		for (int i = 0; i < coefficient.length; i++){
+			for (int j = 0; j < poly.coefficient.length; j++){
 				if (expo[i]==poly.expo[j])
 					len++;
 			}
 			
 		}
 		
-		int max_len = coeff.length + poly.expo.length - len;
+		int max_len = coefficient.length + poly.expo.length - len;
 		int found = 0;
 		int [] final_expo = new int[max_len];
 		double [] final_coeff = new double[max_len];
@@ -110,7 +102,7 @@ public class Polynomial{
 			for (int j = 0; j < poly.expo.length; j++){
 				if (expo[i] == poly.expo[j]){
 					final_expo[f] = expo[i];
-					final_coeff[f] = coeff[i] + poly.coeff[j];
+					final_coeff[f] = coefficient[i] + poly.coefficient[j];
 					found = 1;
 					break;
 				}
@@ -118,7 +110,7 @@ public class Polynomial{
 			}
 			if (found == 0){
 				final_expo[f] = expo[i];
-				final_coeff[f] = coeff[i];
+				final_coeff[f] = coefficient[i];
 				
 			}
 
@@ -134,7 +126,7 @@ public class Polynomial{
 			}
 			if (found == 0){
 				final_expo[f] = poly.expo[i];
-				final_coeff[f] = poly.coeff[i];
+				final_coeff[f] = poly.coefficient[i];
 				f++;				
 			}
 		}
@@ -170,41 +162,51 @@ public class Polynomial{
     public double evaluate(double x){
         double res = 0;
 
-        for (int i = 0; i < coeff.length; i++){
-            res += coeff[i]* Math.pow(x,expo[i]);
+        for (int i = 0; i < coefficient.length; i++){
+            res += coefficient[i]* Math.pow(x,expo[i]);
         }
 
         return res;
 
     }
+    
+	//public int [] multi_expo(int [] arr, int exponent){
+		//int[] new_arr = new int[arr.length];
+		//for (int i = 0; i < arr.length; i++){
+			//new_arr[i] = exponent;
+		//}
+		//return new_arr;
+	//}
 
-    public boolean hasRoot(double x){
-        return 0.0 == evaluate(x);
 
-    }
 
 	public void print(){
 		for (int i = 0; i < expo.length ;i++){
-			System.out.printf("Expo: %d and Coeff: %f\n", expo[i], coeff[i]);
+			System.out.printf("Expo: %d and Coeff: %f\n", expo[i], coefficient[i]);
 
 		}
 
 
 	}
+	
+    public boolean hasRoot(double x){
+        return 0.0 == evaluate(x);
 
-    public Polynomial multiply(Polynomial poly){
+    }
+//new method to add
+    public Polynomial multiply(Polynomial polynomial){
 		Polynomial final_poly = null;
 		
 		for (int i=0; i<expo.length;i++){
 			Polynomial temp = new Polynomial();
 
 			if (expo[i]!=0)
-				temp.expo  = multi_expo(poly.expo, expo[i]);
+				temp.expo  = multi_expo(polynomial.expo, expo[i]);
 			else
-				temp.expo  = poly.expo;
+				temp.expo  = polynomial.expo;
 
 			
-			temp.coeff = add_coeff(poly.coeff, coeff[i]);
+			temp.coefficient = add_coeff(polynomial.coefficient, coefficient[i]);
 			
 			if (final_poly == null) {
 				final_poly = temp;
@@ -240,15 +242,15 @@ public class Polynomial{
 
 		for (int i=0; i <expo.length; i++){
 			if (expo[i] == 0)
-				if (coeff[i] < 0)
-					polyString = polyString + coeff[i];
+				if (coefficient[i] < 0)
+					polyString = polyString + coefficient[i];
 				else
-					polyString = polyString + "+"+ coeff[i];
+					polyString = polyString + "+"+ coefficient[i];
 			else
-				if (coeff[i] < 0)
-					polyString = polyString + coeff[i] + "x" + expo[i];
+				if (coefficient[i] < 0)
+					polyString = polyString + coefficient[i] + "x" + expo[i];
 				else
-					polyString = polyString + "+" + coeff[i] + "x" + expo[i];
+					polyString = polyString + "+" + coefficient[i] + "x" + expo[i];
 		}
 
 		if (polyString.charAt(0)=='+'){
@@ -260,3 +262,9 @@ public class Polynomial{
 
 	}
 }
+
+
+// Add a method named saveToFile that takes one argument of type String representing a
+//file name and saves the polynomial in textual format in the corresponding file (similar to
+//the format used in part d)
+//f. You can add any supplementary classes/methods you might find useful
